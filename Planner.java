448f5330dd44d7eccb6c82488e329d84512c4f62
Todd Baldwin <30380911+tbaldwin8@users.noreply.github.com>
2017-07-23 10:17:31 -0400

    todd

--- Planner.java
+++ Planner.java
@@ -1,16 +1,16 @@
 import java.util.* ;
 public class Planner {
-    private Hashtable<String, String[]> plan; //Planner for a User
+    private Hashtable<String, String> plan; //Planner for a User
      
     public  Planner() {
-       Hashtable<String, String[]> plan = new Hashtable<String, String[]> ();
-       String[] plan1 = new String[3];
-       plan1[0] = "CHM 215 Lecture";
+       Hashtable<String, String> plan = new Hashtable<String, String> ();
+       /* String[] plan1 = new String[3];
+       /plan1[0] = "CHM 215 Lecture";
        plan1[1] = "COS 226 Precept";
        plan1[2] = "MAT 202 Help Session";
        // StdOut.println(Arrays.toString(plan1));
        plan.put("Todd", plan1);
-       // StdOut.println(plan);
+       // StdOut.println(plan); */
        
     
        
@@ -19,12 +19,21 @@ public class Planner {
 
 
 public static void main(String[] args) {
-     Planner plann = new Planner() ;
-     String[] empty = new String[5] ;
+     Planner testPlan = new Planner() ;
+     StdOut.println("Name: ") ;
+     String name = StdIn.readString() ;
+     String temp = "";
+     do {
+         temp = StdIn.readString();
+         testPlan.put(name, temp) ;
+     }while (temp != "End") ;
      
-     for (int i = 0 ; i < 5; i++) {
+    System.out.println(testPlan) ;
+     
+     
+     /*(int i = 0 ; i < 5; i++) {
          StdOut.println("Item " + i); 
          empty[i] = StdIn.readString() ;
-     }
+     }*/
 }
 }
