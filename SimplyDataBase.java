0fc5dee27c6f5c052790a9cabcdd792b84a0b352
ssands1 <ssands@princeton.edu>
2017-07-23 09:34:22 -0400

    attempt5

--- SimplyDataBase.java
+++ SimplyDataBase.java
@@ -1,8 +1,10 @@
-import java.util.*
+import java.util.*;
 
 public class DataBase {
 
 	public static void createDataBase {
-		
+		Hashtable<String, String> users = new Hashtable<String, String>;
+		users.put("Todd", "123a");
+		users.put("Spencer", "123b");
 	}
 }
