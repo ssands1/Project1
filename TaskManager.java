cf39f37f46488f9b930af588c9f449b97585a71e
Todd Baldwin <30380911+tbaldwin8@users.noreply.github.com>
2017-08-01 00:02:23 -0400

    reading input is fucked up, pls help

--- TaskManager.java
+++ TaskManager.java
@@ -1,49 +1,68 @@
 import java.util.* ;
+import java.util.concurrent.TimeUnit;
 public class TaskManager {
     private static Hashtable<Integer, String> taskManager; //TaskManager for user
     
     public  TaskManager() {
-        Hashtable<Integer, String> taskManager = new Hashtable<Integer, String> ();    
-        
+        taskManager = new Hashtable<Integer, String> ();
     }
     
     //place new task as priority n, move all other task down a priority
     public void add_task(Integer priority, String task) {
-      /*  String oldTask = "";
+        String oldTask = "";
         Stack oldTasks = new Stack();
         
         // push all task w/ lower priority than inserted task onto stack
-        for (Integer task_priority: taskManager) {
-            if (task_priority >= priority) {
-                oldTask = taskManager.get(task_priority);
-                oldTasks.push(oldTask);
+        if (taskManager != null) {
+            if (!taskManager.isEmpty()) {
+                Set<Integer> keys = taskManager.keySet();
+                for (Integer key : keys) {
+                    if (key >= priority) {
+                        oldTask = taskManager.get(key);
+                        oldTasks.push((String) oldTask);
+                    }
+                }
+                
+                int size = taskManager.size();
+                
+                // place task back one priortity lower than original
+                for (int i = priority; i < size; i++) {
+                    String temp_task = (String) oldTasks.pop();
+                    taskManager.put (i + 1, temp_task);
+                } 
             }
+            taskManager.put(priority, task);
         }
-        
-        int size = taskManager.size;
-        
-        // place task back one priortity lower than original
-        for (int i = priority; i < size; i++) {
-            String task = oldTask.pop;
-            taskManager*/
     }
+    private static Scanner sc = new Scanner(System.in); // System in
     
     public static void main(String[] args) {
         TaskManager tm = new TaskManager();
-        StdOut.println("Name: ") ;
-        String name = StdIn.readString() ;
-        String task = "";
-        Integer priority = 0;
-        for (int i = 0; !task.equals("End") ; i++) {
+        // System.out.println("Name: ") ;
+        // String name = sc.nextLine() ;
+        Integer priority = 1;
+        for (int i = 0; !priority.equals(0) ; i++) {
+            
             System.out.println("Priority: ");
-            priority = StdIn.readInt();
-            System.out.println("Task: ");
-            task = StdIn.readString();
-            if (taskManager.get(priority) != null) {
-                tm.add_task(priority, task);
-            }
+            priority = sc.nextInt();
+            
+            try{
+                Thread.sleep((long) 1000);
                 
-            taskManager.put(priority, task);
+            }catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+                // code for stopping current task so thread stops
+            }
+            
+            System.out.println("Task: ");
+            String task = sc.nextLine();
+            
+            tm.add_task(priority, task);
+            
+            if (taskManager != null) {
+                taskManager.put(priority, task);
+            }
         }
+        System.out.print(taskManager.toString());
     }
 }
